78f8d39af6dc9e7a33abe2920f7d719944bd93c9
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-01-25 09:15:13 +0000

    Initial commit

---
+++ adv/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="gloa in goals" :key="gloa">
+      <slot :item="gloa" another-prop="..."></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['Finish the COurse', 'Learn Vue']
+    }
+  }
+}
+</script>
