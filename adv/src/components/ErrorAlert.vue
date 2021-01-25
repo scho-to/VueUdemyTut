e6ce28e32214d8545fa685d6d98398f5b89ef25a
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-01-25 11:43:24 +0000

    Finish Section 9, created Vite App for Section 10

---
+++ adv/src/components/ErrorAlert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <dialog open>
+      <slot></slot>
+    </dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+div {
+  position: fixed;
+  background-color: rgba(0,0,0,0.1);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+dialog {
+  margin: 0;
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  background-color: white;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+  padding: 1rem;
+}
+</style>
